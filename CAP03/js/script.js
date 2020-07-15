e56c6902e2289dd985e671a99224e0c43a60b5e4
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2020-07-15 15:37:01 -0300

    Alerta p boas práticas

--- CAP03/js/script.js
+++ CAP03/js/script.js
@@ -9,6 +9,7 @@ city.textContent = 'Nova Cidade';
 var personalDataArray = document.querySelector('.personal-data');
 console.log(personalDataArray);
 
+//obs: é possivel alterar o css in js mas nao é boas práticas
 var data = Array.from(document.querySelectorAll('.data'));
 for (var i = 0; i < data.length; i++) {
   var currentElement = data[i];
