9261277e506320d148b9d20df698a06f459b10ab
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2020-07-15 12:57:54 -0300

    revisão blocos

--- CAP02/aula6/producao/js/script.js
+++ CAP02/aula6/producao/js/script.js
@@ -1,4 +1,77 @@
-console.log('Olá, mundo!');
+//console.log('Olá, mundo!');
 
-var title = document.querySelector('h1');
-title.textContent = 'Modificado por Thuane de Souza';
+//var title = document.querySelector('h1');
+//title.textContent = 'Modificado por Thuane de Souza';
+
+//revisão
+var dia = 1;
+if (dia === 1) {
+  console.log('Domingo');
+} else {
+  if (dia === 2) {
+    console.log('Segunda');
+  } else {
+    if (dia === 3) {
+      console.log('Terça');
+    } else {
+      if (dia === 4) {
+        console.log('Terça');
+      }
+    }
+  }
+}
+// parando aqui para usar o switch
+var r = '';
+switch (dia) {
+  case 1:
+    r = 'Domingo';
+    break;
+  case 2:
+    r = 'Segunda';
+    break;
+  case 3:
+    r = 'Terça';
+    break;
+  case 4:
+    r = 'Quarta';
+    break;
+  case 5:
+    r = 'Quinta';
+    break;
+  case 6:
+    r = 'Sexta';
+    break;
+  case 7:
+    r = 'Sabado';
+    break;
+}
+console.log(r);
+
+//operador ternario
+a = 6;
+b = 7;
+var resposta = a > b ? 'Maior' : a < b ? 'Menor' : 'Igual';
+console.log(resposta);
+//obs: não recomendado no dia a dia mas é um bom exercicio
+var diaSemana =
+  dia === 1
+    ? 'Domingo'
+    : dia === 2
+    ? 'Segunda'
+    : dia === 3
+    ? 'Terça'
+    : dia === 4
+    ? 'Terça'
+    : 'Outro dia';
+console.log(diaSemana);
+
+//somatorio
+function superSum(from, upTo) {
+  var sum = 0;
+  for (var i = from; i < upTo; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(superSum(1, 5));
